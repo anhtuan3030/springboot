acd81973c6e0129c85fb4dfbdfabba0756e8674d
shameera-a <Shameera.A@synergenhealth.com>
2019-09-10 15:44:28 +0530

    add new controller method for page loading

--- loginregister/src/main/java/com/example/demo/AppController.java
+++ loginregister/src/main/java/com/example/demo/AppController.java
@@ -1,27 +1,39 @@
 package com.example.demo;
-	import java.util.List;
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.ui.Model;
-	import org.springframework.web.bind.annotation.ModelAttribute;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import com.example.demo.ProductService;
-	@Controller
-	public class AppController {	
-		@Autowired
-		private ProductService service; 	
-		@RequestMapping("/ChartLine")
-	    public String PostForm(@ModelAttribute(value="greeting") Greeting greeting,Model model) {		
-				model.addAttribute("greeting", greeting);
-				String time = greeting.getTime();
-				String date = greeting.getDate();
-				System.out.println(time);
-				System.out.println(date);
-				String date1 = date.replace('-','/');
-				List<Object[]> listData = service.listData(time,date1);
-				model.addAttribute("listData", listData);
-				List<Object[]> listTime = service.listTime(time,date1);
-				model.addAttribute("listTime", listTime);		
-				return "ChartLine";		
-	    }	
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/ChartLine")
+    public String ChartLineFormView(Model model){
+        model.addAttribute("greeting",new Greeting());
+    	return "ChartLine";
 	}
+
+	@RequestMapping("/ReqChartLine")
+    public String PostForm(@Valid @ModelAttribute(value = "greeting") Greeting greeting, BindingResult result, Model model) {
+        model.addAttribute("greeting", greeting);
+        String time = greeting.getTime();
+        String date = greeting.getDate();
+        System.out.println(time);
+        System.out.println(date);
+        String date1 = date.replace('-', '/');
+        List<Object[]> listData = service.listData(time, date1);
+        model.addAttribute("listData", listData);
+        List<Object[]> listTime = service.listTime(time, date1);
+        model.addAttribute("listTime", listTime);
+        return "ChartLine";
+    }
+}
